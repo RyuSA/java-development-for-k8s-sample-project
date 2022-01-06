2d393e2196bf2553f24444c3a39dc9e6984ef1ca
RyuSA <kommsussertad@gmail.com>
2022-01-06 20:33:50 +0900

    init

---
+++ src/main/java/com/github/ryusa/vulture/javafork8s/JavaForK8sApplication.java
@@ -0,0 +1,13 @@
+package com.github.ryusa.vulture.javafork8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaForK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaForK8sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ryusa/vulture/javafork8s/JavaForK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ryusa.vulture.javafork8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaForK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
